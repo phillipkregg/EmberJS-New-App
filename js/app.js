3a6ed3d1b29509104ad4696e3dbcdf6a882a8bca
Phillip Lackey <dp078971@MBP-C02NJ4G4G3QN.local>
2016-05-30 13:09:23 -0500

    initial commit

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ App = Ember.Application.create();
 
 App.Router.map(function() {
   // put your routes here
+  this.route('about');
+  this.route('contact');
 });
 
 App.IndexRoute = Ember.Route.extend({
